9f03765ad21f4f1ee13e6648fb51b0cfde5f81ca
Ricky Sidhu <seeingsaturn@gmail.com>
2016-09-18 15:35:46 -0700

    step 1c

--- lib/lixical/porter_stemmer.ex
+++ lib/lixical/porter_stemmer.ex
@@ -28,16 +28,16 @@ defmodule Lixical.PorterStemmer do
   """
   def step_1a(input) do
     cond do
-      String.ends_with?(input, "sses") ->
+      ends_with?(input, "sses") ->
         String.replace_suffix(input, "sses", "ss")
 
-      String.ends_with?(input, "ies") ->
+      ends_with?(input, "ies") ->
         String.replace_suffix(input, "ies", "i")
 
-      String.ends_with?(input, "ss") ->
+      ends_with?(input, "ss") ->
         String.replace_suffix(input, "ss", "s")
 
-      String.ends_with?(input, "s") ->
+      ends_with?(input, "s") ->
         String.replace_suffix(input, "s", "")
 
       true ->
@@ -55,19 +55,59 @@ defmodule Lixical.PorterStemmer do
   """
   def step_1b(input) do
     cond do
-      String.ends_with?(input, "eed") && measure(input) > 0 ->
+      ends_with?(input, "eed") && measure(input) > 0 ->
         String.replace_suffix(input, "eed", "ee")
 
-      String.ends_with?(input, "ed") && contains_vowel?(input) ->
-        String.replace_suffix(input, "ed", "")
+      ends_with?(input, "ed") && contains_vowel?(input) ->
+        result = String.replace_suffix(input, "ed", "")
+        if result !== input, step_1b_additional(result)
 
-      String.ends_with?(input, "ing") && contains_vowel?(input) ->
+      ends_with?(input, "ing") && contains_vowel?(input) ->
         String.replace_suffix(input, "ing", "")
+        if result !== input, step_1b_additional(result)
+
+      true ->
+        input
+    end
+  end
+
+  @doc """
+    AT -> ATE                       conflat(ed)  ->  conflate
+    BL -> BLE                       troubl(ed)   ->  trouble
+    IZ -> IZE                       siz(ed)      ->  size
+    (*d and not (*L or *S or *Z))
+       -> single letter
+                                    hopp(ing)    ->  hop
+                                    tann(ed)     ->  tan
+                                    fall(ing)    ->  fall
+                                    hiss(ing)    ->  hiss
+                                    fizz(ed)     ->  fizz
+    (m=1 and *o) -> E               fail(ing)    ->  fail
+                                    fil(ing)     ->  file
+  """
+  def step_1b_additional(input) do
+    cond do
+      ends_with?(input, "at") ->
+        String.replace_suffix(input, "at", "ate")
+
+      ends_with?(input, "bl") ->
+        String.replace_suffix(input, "bl", "ble")
+
+      ends_with?(input, "iz") ->
+        String.replace_suffix(input, "iz", "ize")
+
+      ends_with_double_consonant?(input) && does_not_end_with(input, ['l', 's', 'z']) ->
+        String.slice(input, 0..-2)
+
+      (measure(input) == 1) && ends_with_cvc?(input) && does_not_end_with?(input, ['w', 'x', 'y']) ->
+        input <> "e"
     end
   end
 
   def step_1c(input) do
-    input
+    if contains_vowel?(input) && ends_with?(input, "y")
+      String.replace_suffix(input, "y", "i")
+
   end
 
   def stems([]), do: []
